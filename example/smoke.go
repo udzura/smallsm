0f82c78018865f53ce16b074b5fc996f7586b5aa
Uchio Kondo <udzura@udzura.jp>
2023-04-16 21:05:35 +0900

    Use rbt

--- example/smoke.go
+++ example/smoke.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/udzura/smallsm"
+)
 
 func main() {
-	fmt.Println("It does not smoke")
+	db := smallsm.New(os.TempDir() + "/smallsm-1")
+
+	db.Put("Test", "Hello")
+	db.Put("Test2", "Hello2")
+	db.Put("Test3", "Hello3")
+
+	got, _ := db.Get("Test")
+	fmt.Println("Test =", got)
 }
--- smallsm.go
+++ smallsm.go
@@ -14,12 +14,18 @@ type SmalLSM struct {
 	memtableWorking *SmalMemTable
 	sstables        []*SmalSSTable
 
+	logdir string
 	// TODO: compaction
 	// inCompaction bool
 }
 
 type SmalMemTable struct {
 	tree rbt.Tree
+}
+
+type SmalLog struct {
+	Key, Value string
+	Deleted    bool
 }
 
 type SmalSSTable struct {
@@ -29,4 +35,56 @@ type SmalSSTable struct {
 
 	file               *os.File
 	dataLoadedOnMemory bool
+}
+
+func New(dir string) *SmalLSM {
+	os.MkdirAll(dir, 0700)
+	memTable := &SmalMemTable{
+		tree: *rbt.NewWithStringComparator(),
+	}
+
+	return &SmalLSM{
+		memTable:        memTable,
+		memtableWorking: nil,
+		sstables:        make([]*SmalSSTable, 0),
+		logdir:          dir,
+	}
+}
+
+func (lsm *SmalLSM) Get(key string) (string, bool) {
+	lsm.mu.RLock()
+	defer lsm.mu.RUnlock()
+	data, ok := lsm.memTable.tree.Get(key)
+	if !ok {
+		return "", false
+	}
+	log := data.(*SmalLog)
+	if log.Deleted {
+		return "", false
+	}
+
+	return log.Value, true
+}
+
+func (lsm *SmalLSM) Put(key, value string) {
+	lsm.mu.Lock()
+	defer lsm.mu.Unlock()
+	log := &SmalLog{
+		Key:   key,
+		Value: value,
+	}
+	lsm.memTable.tree.Put(key, log)
+}
+
+func (lsm *SmalLSM) Delete(key string) {
+	lsm.mu.Lock()
+	defer lsm.mu.Unlock()
+	lsm.mu.Lock()
+	defer lsm.mu.Unlock()
+	log := &SmalLog{
+		Key:     key,
+		Value:   "",
+		Deleted: true,
+	}
+	lsm.memTable.tree.Put(key, log)
 }
