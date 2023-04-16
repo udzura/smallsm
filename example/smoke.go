4769ddfb9db10a5f097dfa8f10b36f3a49c09e8f
Uchio Kondo <udzura@udzura.jp>
2023-04-16 22:03:21 +0900

    Basic sstable generation

--- example/smoke.go
+++ example/smoke.go
@@ -8,7 +8,10 @@ import (
 )
 
 func main() {
-	db := smallsm.New(os.TempDir() + "/smallsm-1")
+	db, err := smallsm.New(os.TempDir() + "/smallsm-1")
+	if err != nil {
+		panic(err)
+	}
 
 	db.Put("Test", "Hello")
 	db.Put("Test2", "Hello2")
@@ -16,4 +19,39 @@ func main() {
 
 	got, _ := db.Get("Test")
 	fmt.Println("Test =", got)
+	got, _ = db.Get("Test2")
+	fmt.Println("Test2 =", got)
+	got, _ = db.Get("Test3")
+	fmt.Println("Test3 =", got)
+
+	if err := db.MigrateToSSTable(); err != nil {
+		panic(err)
+	}
+
+	got, _ = db.Get("Test")
+	fmt.Println("Test =", got)
+	got, _ = db.Get("Test2")
+	fmt.Println("Test2 =", got)
+	got, _ = db.Get("Test3")
+	fmt.Println("Test3 =", got)
+
+	db.Delete("Test3")
+	got, _ = db.Get("Test3")
+	fmt.Println("Test3 =", got)
+
+	if err := db.MigrateToSSTable(); err != nil {
+		panic(err)
+	}
+
+	got, _ = db.Get("Test3")
+	fmt.Println("Test3 =", got)
+
+	// reopem
+	db, _ = smallsm.New(os.TempDir() + "/smallsm-1")
+	got, _ = db.Get("Test")
+	fmt.Println("Test =", got)
+	got, _ = db.Get("Test2")
+	fmt.Println("Test2 =", got)
+	got, _ = db.Get("Test3")
+	fmt.Println("Test3 =", got)
 }
--- smallsm.go
+++ smallsm.go
@@ -1,8 +1,13 @@
 package smallsm
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
+	"path/filepath"
+	"sort"
 	"sync"
+	"time"
 
 	rbt "github.com/emirpasic/gods/trees/redblacktree"
 )
@@ -24,41 +29,96 @@ type SmalMemTable struct {
 }
 
 type SmalLog struct {
-	Key, Value string
-	Deleted    bool
+	Key     string `json:"k"`
+	Value   string `json:"v"`
+	Deleted bool   `json:"d"`
 }
 
 type SmalSSTable struct {
-	prefix              string
-	index               map[string]int64
-	indexLoadedOnMemory bool
-
-	file               *os.File
-	dataLoadedOnMemory bool
+	prefix string
+	index  map[string]int64
+	file   *os.File
 }
 
-func New(dir string) *SmalLSM {
-	os.MkdirAll(dir, 0700)
+func New(dir string) (*SmalLSM, error) {
+	os.MkdirAll(dir, 0o0700)
 	memTable := &SmalMemTable{
 		tree: *rbt.NewWithStringComparator(),
+	}
+
+	sstables := make([]*SmalSSTable, 0)
+	logs, err := filepath.Glob(filepath.Join(dir, "*.log"))
+	if err != nil {
+		return nil, err
+	}
+	sort.Sort(sort.Reverse(sort.StringSlice(logs)))
+	for _, path := range logs {
+		prefix := filepath.Base(path)
+		prefix = prefix[0 : len(prefix)-4] // chop `.log`
+		sst, err := LoadSSTable(dir, prefix)
+		if err != nil {
+			return nil, err
+		}
+		sstables = append(sstables, sst)
 	}
 
 	return &SmalLSM{
 		memTable:        memTable,
 		memtableWorking: nil,
-		sstables:        make([]*SmalSSTable, 0),
+		sstables:        sstables,
 		logdir:          dir,
+	}, nil
+}
+
+func LoadSSTable(dir, prefix string) (*SmalSSTable, error) {
+	sst := &SmalSSTable{
+		prefix: prefix,
+		index:  make(map[string]int64),
 	}
+
+	indexf, err := os.Open(filepath.Join(dir, prefix+".index"))
+	if err != nil {
+		return nil, err
+	}
+	defer indexf.Close()
+	if err := json.NewDecoder(indexf).Decode(&sst.index); err != nil {
+		return nil, err
+	}
+	f, err := os.Open(filepath.Join(dir, prefix+".log"))
+	sst.file = f
+
+	return sst, nil
 }
 
 func (lsm *SmalLSM) Get(key string) (string, bool) {
 	lsm.mu.RLock()
 	defer lsm.mu.RUnlock()
+	var log *SmalLog = nil
 	data, ok := lsm.memTable.tree.Get(key)
 	if !ok {
+		if lsm.memtableWorking != nil {
+			data, ok := lsm.memtableWorking.tree.Get(key)
+			if ok {
+				log = data.(*SmalLog)
+			}
+		}
+
+		if log == nil {
+			for _, sst := range lsm.sstables {
+				data, ok := sst.Get(key)
+				if ok {
+					log = data
+					break
+				}
+			}
+		}
+	} else {
+		log = data.(*SmalLog)
+	}
+	if log == nil {
 		return "", false
 	}
-	log := data.(*SmalLog)
+
 	if log.Deleted {
 		return "", false
 	}
@@ -79,12 +139,102 @@ func (lsm *SmalLSM) Put(key, value string) {
 func (lsm *SmalLSM) Delete(key string) {
 	lsm.mu.Lock()
 	defer lsm.mu.Unlock()
-	lsm.mu.Lock()
-	defer lsm.mu.Unlock()
 	log := &SmalLog{
 		Key:     key,
 		Value:   "",
 		Deleted: true,
 	}
 	lsm.memTable.tree.Put(key, log)
+}
+
+func (lsm *SmalLSM) MigrateToSSTable() error {
+	lsm.mu.Lock()
+	lsm.memtableWorking = lsm.memTable
+	lsm.memTable = &SmalMemTable{
+		tree: *rbt.NewWithStringComparator(),
+	}
+	lsm.mu.Unlock()
+
+	prefix := fmt.Sprintf("%024d", time.Now().UnixNano())
+	sst := &SmalSSTable{
+		prefix: prefix,
+		index:  make(map[string]int64),
+	}
+	logf, err := os.OpenFile(
+		filepath.Join(lsm.logdir, prefix+".log"),
+		os.O_CREATE|os.O_TRUNC|os.O_WRONLY,
+		0o0600,
+	)
+	if err != nil {
+		return err
+	}
+	defer logf.Close()
+	logWriter := json.NewEncoder(logf)
+	indexf, err := os.OpenFile(
+		filepath.Join(lsm.logdir, prefix+".index"),
+		os.O_CREATE|os.O_TRUNC|os.O_WRONLY,
+		0o0600,
+	)
+	if err != nil {
+		return err
+	}
+	defer indexf.Close()
+
+	for _, key := range lsm.memtableWorking.tree.Keys() {
+		key := key.(string)
+		data, ok := lsm.memtableWorking.tree.Get(key)
+		if !ok {
+			continue
+		}
+		log := data.(*SmalLog)
+		offset, err := logf.Seek(0, 1)
+		if err != nil {
+			return err
+		}
+		if err := logWriter.Encode(log); err != nil {
+			return err
+		}
+
+		sst.index[key] = offset
+	}
+
+	if err := json.NewEncoder(indexf).Encode(&sst.index); err != nil {
+		return err
+	}
+	_ = logf.Close()
+
+	// reopen
+	f, err := os.Open(filepath.Join(lsm.logdir, prefix+".log"))
+	if err != nil {
+		return err
+	}
+
+	sst.file = f
+
+	lsm.mu.Lock()
+	lsm.sstables = append([]*SmalSSTable{sst}, lsm.sstables...)
+	lsm.memtableWorking = nil
+	lsm.mu.Unlock()
+
+	return nil
+}
+
+func (sst *SmalSSTable) Get(key string) (*SmalLog, bool) {
+	off, ok := sst.index[key]
+	if !ok {
+		return nil, false
+	}
+	_, err := sst.file.Seek(off, 0)
+	if err != nil {
+		// TODO when cannnot open
+		panic(err)
+	}
+	defer sst.file.Seek(0, 0)
+
+	log := &SmalLog{}
+	if err := json.NewDecoder(sst.file).Decode(log); err != nil {
+		panic(err)
+	}
+
+	return log, true
 }
